dec4c78495bcac1238f4bafc24933581a8699ab1
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-05-06 10:04:22 +0000

    'puppet linting '

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#  install flask from pip3.
-
+ A puppet manifest installing flask v2.1.0 which is a package from pip3.
 package { 'flask':
   ensure   => '2.1.0',
-  provider => pip3,
+  provider => 'pip3'
 }
