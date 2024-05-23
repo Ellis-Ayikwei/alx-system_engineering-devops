a7d3e458022d46033b81a753fb7cf087df2cbd66
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-05-23 01:31:10 +0000

    eidted the indetation

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -13,19 +13,19 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     # usr_id = int(sys.argv[1])
     usrs = req.get(url + 'users').json()
-   
     # print(to_do)
 
     # # open write the data into a csv file using the user id
     with open("todo_all_employees.json", "w", newline="") as jsonfile:
         tasklist = []
         for usr in usrs:
-            to_dos = req.get(url + 'todos', params={"userId": usr.get("id")}).json()
-            for task in to_dos:
-                tasklist.append({usr.get("id"): [{
-                    "username": usr.get("username"),
-                    "task": task.get("title"),
-                    "completed": task.get("completed")
-                }]})
+            to_dos = req.get(url + 'todos',
+                             params={"userId": usr.get("id")}).json()
+        for task in to_dos:
+            tasklist.append({usr.get("id"): [{
+                "username": usr.get("username"),
+                "task": task.get("title"),
+                "completed": task.get("completed")
+            }]})
 
         json.dump(tasklist, jsonfile)
