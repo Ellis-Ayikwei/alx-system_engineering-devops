f2e35e1a4eda1bbeb360d8e8bbf71940b0d95e5b
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-06-04 15:38:40 +0000

    the top en reddits

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""a module that defines the top_ten"""
+import requests
+
+
+def top_ten(subreddit):
+    """Return the top ten reddits posts of a subreddit.
+
+    arguments:
+        subreddit -- the subreddit to be checked
+    Return: the top ten reddits posts 
+    """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(x.get("data").get('title')) for x in results.get("children")]
