6f561a0434576f420c618be90c857c8f22217843
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-06-04 15:01:01 +0000

    get me the number of subreddits

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit.
+
+    arguments:
+        subreddit -- the subreddit to be checked
+    Return: the number of subscribers pf the subreddit
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code == 404:
+        return 0
+    results = res.json().get("data")
+    return results.get("subscribers")
