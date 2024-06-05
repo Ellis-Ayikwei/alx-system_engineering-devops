24f0bc575add20cfde6f5ee0e0ea77153b683c46
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-06-05 04:58:39 +0000

    edite dth white space

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,7 +5,6 @@ import requests
 
 def top_ten(subreddit):
     """Return the top ten reddits posts of a subreddit."""
-
     url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
     useraAgent = requests.get('https://httpbin.org/user-agent').json()
     useraAgent = useraAgent['user-agent']
