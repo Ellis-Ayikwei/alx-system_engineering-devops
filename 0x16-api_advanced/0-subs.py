af6bc267650f799bef9ba9beee6da67e3cc4c083
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-06-05 13:02:38 +0000

    test to see if the check still works

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
-"""Function that queries the Reddit API & returns the number of subscribers"""
+"""0-subs module"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    useraAgent = requests.get('https://httpbin.org/user-agent').json()
-    useraAgent = useraAgent['user-agent']
+    """
+    Queries the reddit API
+    Return: 
+        the number of subscribers
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": useraAgent
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-
-    try:
-        res = requests.get(url, headers=headers, allow_redirects=False)
-        if res.status_code != 200:
-            return 0
-
-        results = res.json().get("data")
-        return results.get("subscribers", 0)
-
-    except requests.RequestException:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    except ValueError:
-        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
