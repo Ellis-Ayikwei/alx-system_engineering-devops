9afedef08bc2acd3f5f4ea28664d5dd95eab5b9c
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-06-05 02:21:15 +0000

    the sorted keywords

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""the count module"""
+import requests
+
+
+def count_words(subreddit, word_list, instances={}, after="", count=0):
+    """
+    recursive function that queries the Reddit API, parses the title of all hot
+    articles, and prints a sorted count of
+    given keywords (case-insensitive, delimited by spaces. Javascript should
+    count as javascript, but java should not).
+    Args:
+        subreddit (str): The subreddit to query.
+        word_list (list): The list of words to search for in post titles.
+        instances (obj): Key/value pairs of words/counts.
+        after (str): The parameter for the next page of the API results.
+        count (int): The parameter of results matched per query .
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    res = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    try:
+        results = res.json()
+        if res.status_code != 200:
+            raise Exception
+    except Exception:
+        print("")
+        return
+
+    results = results.get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for child in results.get("children"):
+        title = child.get("data").get("title").lower().split()
+        for a_word in word_list:
+            if a_word.lower() in title:
+                times = len([t for t in title if t == a_word.lower()])
+                if instances.get(a_word) is None:
+                    instances[a_word] = times
+                else:
+                    instances[a_word] += times
+
+    if after is None:
+        if len(instances) == 0:
+            print("")
+            return
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print(f"{k}: {v}") for k, v in instances]
+    else:
+        count_words(subreddit, word_list, instances, after, count)
