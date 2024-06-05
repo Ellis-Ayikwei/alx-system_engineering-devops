05cf969662389e87b9b2ab584fa64341e9a7019d
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-06-05 12:54:47 +0000

    edite the docstring

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and returns the number of subscribers"""
+"""Function that queries the Reddit API & returns the number of subscribers"""
 import requests
 
 
