f76346a4a659c30c895b20c23cdc580ba29a8623
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-06-06 01:43:45 +0000

    edited the code for the fix server error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #fixes an error in a server 
 
-file { '/var/www/html/wp-settings.php' :
-  ensure => present,
-  content => replace(/phpp/g, 'php'),
+exec { 'fix-server-error':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
