4b19851e91c7475c0ca9624d3c9be7ddd2268658
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-06-16 04:21:38 +0000

    iincrease the limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,12 @@
-# Fix for nginx
+#request limit at nginx
+
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/worker_processes  1/worker_processes  auto/g" /etc/nginx/nginx.conf',
-  user    => 'root',
-  notify  => 'Service[nginx]',
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+#Nginx Restart
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,12 @@
-# Change OS configuration for holberton user
-exec { 'change-os-configuration-for-holberton-user':
-  command => 'ulimit -n 2048',
-  user    => 'root',
+# fix limit file at hoberton user.
+
+exec { 'fix_limit_hoberton_user':
+  command => 'sed -i "/holberton hard/s/5/10000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit hoberton user.
+exec { 'increase_soft_file':
+  command => 'sed -i "/holberton soft/s/4/20000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
